89bc67dcd390bb8cc34913e67d04a7c00dc41804
Freddy Kristiansen <fkr@freddy.dk>
2026-06-18 09:07:25 +0200

    use projectmadeira

--- .github/DeployToTestEnvironment.ps1
+++ .github/DeployToTestEnvironment.ps1
@@ -31,6 +31,7 @@ if (-not $appsList -or $appsList.Count -eq 0) {
 
 try {
     $authContext = $parameters.AuthContext | ConvertFrom-Json | ConvertTo-HashTable
+    $authContext."Scopes" = 
     $bcAuthContext = New-BcAuthContext @authContext
     if ($null -eq $bcAuthContext) {
         throw "Authentication failed"
@@ -52,7 +53,7 @@ $sandboxEnvironment = ($response.environmentType -eq 1)
 if (-not $sandboxEnvironment) {
     throw "Environment $($environmentName) is not a sandbox environment. Deployment can only be done to sandbox environments."
 }
-$scope = $parameters."Scope"
+$scope = $parameters."scopes" = "https://projectmadeira.com/.default")
 if (-not $scope) {
     $scope = "DEV"
 }
